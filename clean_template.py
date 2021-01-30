1eeba4be8ee8ae0d2aec7687df969c5cee0d0bfb
tomMoral <thomas.moreau.2010@gmail.com>
2021-01-30 11:11:42 +0100

    CLN rename clean_template script

--- clean_template.py
+++ clean_template.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     # Get the name of the repo and parse org and bench name
     repo_url = check_output(['git', 'remote', 'get-url', 'origin']).decode()
-    name = re.split('github.com[:/]', repo_url)[1]
+    name = re.split('github.com[:/]', repo_url)[1].strip()
     ORG, BENCHMARK_NAME = name.replace('.git', '').split('/')
 
     # update readme to have the right badge
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     text = text.replace('#BENCHMARK_NAME', BENCHMARK_NAME)
 
     text = '\n'.join([line for line in text.splitlines()[13:]
-                      if 'template_benchmark' not in line])
+                      if 'template_benchmark' not in line] + [''])
 
     README.write_text(text)
