d24e3c6cded86a33cc551448ba170d5c906af78b
tomMoral <thomas.moreau.2010@gmail.com>
2021-09-15 16:52:27 +0200

    FIX check_test name in test_config

--- test_config.py
+++ test_config.py
@@ -3,7 +3,7 @@ import sys  # noqa: F401
 import pytest  # noqa: F401
 
 
-def xfail_test_solver_install(solver_class):
+def check_test_solver_install(solver_class):
     """Hook called in `test_solver_install`.
 
     If one solver needs to be skip/xfailed on some
