2344bd00c529da40e2053115d90a568174f7d22a
tomMoral <thomas.moreau.2010@gmail.com>
2022-11-22 00:45:38 +0100

    DOC improve description of elements in template_benchmark

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,33 +1,39 @@
-import numpy as np
-
-from benchopt import BaseDataset
+from benchopt import BaseDataset, safe_import_context
 
 
+# Protect the import with `safe_import_ctx`. This allows:
+# - To skip import to fasten autocompletion for instance.
+# - To get requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    import numpy as np
+
+
+# All datasets must be name `Dataset` and derive from the `BaseDataset`
 class Dataset(BaseDataset):
 
+    # Name to select the dataset in the CLI and to display the results.
     name = "Simulated"
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
     parameters = {
         'n_samples, n_features': [
             (1000, 500),
-            (5000, 200)]
+            (5000, 200),
+        ],
+        'random_state': [27],
     }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
-        # Store the parameters of the dataset
-        self.n_samples = n_samples
-        self.n_features = n_features
-        self.random_state = random_state
-
     def get_data(self):
+        # The return arguments of this function are passed as keyword arguments
+        # to `Objective.set_data`. This defines the benchmark's
+        # API to pass data. This is customizable for each benchmark.
 
+        # Generate data using `numpy`.
         rng = np.random.RandomState(self.random_state)
         X = rng.randn(self.n_samples, self.n_features)
         y = rng.randn(self.n_samples)
 
-        # `data` holds the keyword arguments for the `set_data` method of the
-        # objective.
-        # They are customizable.
+        # `data` defines the keyword arguments for `Objective.set_data`.
         return dict(X=X, y=y)
