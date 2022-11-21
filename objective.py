e71c2ccd9dc61f3696e9f9b9c8bfc4cd2e68736e
tomMoral <thomas.moreau.2010@gmail.com>
2022-11-22 00:45:38 +0100

    DOC improve description of elements in template_benchmark

--- objective.py
+++ objective.py
@@ -1,14 +1,21 @@
 from benchopt import BaseObjective, safe_import_context
 
-# Protect import to allow manipulating objective without importing library
-# Useful for autocompletion and install commands
+# Protect the import with `safe_import_ctx`. This allows:
+# - To skip import to fasten autocompletion for instance.
+# - To get requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
 
 
+# The benchmark objective must be name `Objective` and
+# derive from `BaseObjective` for `benchopt` to work properly.
 class Objective(BaseObjective):
+
+    # Name to select the objective in the CLI and to display the results.
     name = "Ordinary Least Squares"
 
+    # List of parameters for the objective. The benchmark will consider
+    # the cross product for each key in the dictionary.
     # All parameters 'p' defined here are available as 'self.p'.
     # This means the OLS objective will have a parameter `self.whiten_y`.
     parameters = {
@@ -17,31 +24,43 @@ class Objective(BaseObjective):
 
     # Minimal version of benchopt required to run this benchmark.
     # Bump it up if the benchmark depends on a new feature of benchopt.
-    min_benchopt_version = "1.2.1"
+    min_benchopt_version = "1.3"
 
     def set_data(self, X, y):
         # The keyword arguments of this function are the keys of the `data`
-        # dict in the `get_data` function of the dataset.
-        # They are customizable.
+        # dict returned by `Dataset.get_data`. This defines the benchmark's
+        # API to pass data. This is customizable for each benchmark.
         self.X, self.y = X, y
 
-        # if whiten_y is True, remove the mean of `y`.
+        # `set_data` can be used to preprocess the data. For instance,
+        # if `whiten_y` is True, remove the mean of `y`.
         if self.whiten_y:
             y -= y.mean(axis=0)
 
     def compute(self, beta):
         # The arguments of this function are the outputs of the
-        # `get_result` method of the solver.
-        # They are customizable.
+        # `Sovler.get_result`. This defines the benchmark's API to pass
+        # solvers' result. This is customizable for each benchmark.
         diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+
+        # This method can return many metrics in a dictionary. One of these
+        # metrics needs to be `value` for convergence detection purposes.
+        return dict(
+            value=.5 * diff.dot(diff),
+        )
 
     def get_one_solution(self):
-        # Return one solution. This should be compatible with 'self.compute'.
+        # Return one solution. This should be an object compatible
+        # with `self.compute`. This is mainly for testing purposes.
         return np.zeros(self.X.shape[1])
 
     def get_objective(self):
+        # Define the information to pass to each solver to run the benchmark.
         # The output of this function are the keyword arguments
-        # for the `set_objective` method of the solver.
-        # They are customizable.
-        return dict(X=self.X, y=self.y)
+        # for the `set_objective` method of the solver. This defines the
+        # benchmark's API for passing the objective to the solver.
+        # This is customizable for each benchmark.
+        return dict(
+            X=self.X,
+            y=self.y,
+        )
--- solvers/python-gd.py
+++ solvers/python-gd.py
@@ -1,20 +1,32 @@
-import numpy as np
+from benchopt import BaseSolver, safe_import_context
+
+# Protect the import with `safe_import_ctx`. This allows:
+# - To skip import to fasten autocompletion for instance.
+# - To get requirements info when all dependencies are not installed.
+with safe_import_context() as import_ctx:
+    import numpy as np
 
 
-from benchopt import BaseSolver
-
-
+# The benchmark solvers must be name `Solver` and
+# derive from `BaseSolver` for `benchopt` to work properly.
 class Solver(BaseSolver):
-    """Gradient descent solver, optionally accelerated."""
+
+    # Name to select the solver in the CLI and to display the results.
     name = 'GD'
 
-    # any parameter defined here is accessible as a class attribute
-    parameters = {'scale_step': [1, 1.99]}
+    # List of parameters for the sovler. The benchmark will consider
+    # the cross product for each key in the dictionary.
+    # All parameters 'p' defined here are available as 'self.p'.
+    parameters = {
+        'scale_step': [1, 1.99],
+    }
 
     def set_objective(self, X, y):
+        # Define the information received by each solver from the objective.
         # The arguments of this function are the results of the
-        # `get_objective` method of the objective.
-        # They are customizable.
+        # `Objective.get_objective`. This defines the benchmark's API for
+        # passing the objective to the solver.
+        # This is customizable for each benchmark.
         self.X, self.y = X, y
 
     def run(self, n_iter):
@@ -30,7 +42,8 @@ class Solver(BaseSolver):
         self.w = w
 
     def get_result(self):
+        # Return the result from one optimization run.
         # The outputs of this function are the arguments of the
-        # `compute` method of the objective.
-        # They are customizable.
+        # `compute` method of the objective. This defines the benchmark's
+        # API for solvers' results. This is customizable for each benchmark.
         return self.w
