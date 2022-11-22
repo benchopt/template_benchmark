53605bb7923dd6a8ffc85a3897185ad2c7a9717d
mathurinm <mathurin.massias@gmail.com>
2022-11-22 09:36:13 +0100

    comments in simulated

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -2,13 +2,13 @@ from benchopt import BaseDataset, safe_import_context
 
 
 # Protect the import with `safe_import_ctx`. This allows:
-# - To skip import to fasten autocompletion for instance.
+# - To skip import to speed up autocompletion in CLI.
 # - To get requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     import numpy as np
 
 
-# All datasets must be name `Dataset` and derive from the `BaseDataset`
+# All datasets must be named `Dataset` and inherit from `BaseDataset`
 class Dataset(BaseDataset):
 
     # Name to select the dataset in the CLI and to display the results.
@@ -16,7 +16,7 @@ class Dataset(BaseDataset):
 
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
-    # All parameters 'p' defined here are available as 'self.p'.
+    # Any parameters 'param' defined here is available as `self.param`.
     parameters = {
         'n_samples, n_features': [
             (1000, 500),
@@ -28,12 +28,12 @@ class Dataset(BaseDataset):
     def get_data(self):
         # The return arguments of this function are passed as keyword arguments
         # to `Objective.set_data`. This defines the benchmark's
-        # API to pass data. This is customizable for each benchmark.
+        # API to pass data. It is customizable for each benchmark.
 
-        # Generate data using `numpy`.
+        # Generate pseudorandom data using `numpy`.
         rng = np.random.RandomState(self.random_state)
         X = rng.randn(self.n_samples, self.n_features)
         y = rng.randn(self.n_samples)
 
-        # `data` defines the keyword arguments for `Objective.set_data`.
+        # The dictionary defines the keyword arguments for `Objective.set_data`
         return dict(X=X, y=y)
