cebcb6bc43d11348df3f509370b023bf3bac59b8
Ambroise Odonnat <ambroiseodonnattechnologie@gmail.com>
2024-06-06 14:41:46 +0200

    DOC benchmark_utils/__init__.py (#38)
    Co-authored-by: mathurinm <mathurin.massias@gmail.com>

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -1,7 +1,18 @@
 # `benchmark_utils` is a module in which you can define code to reuse in
 # the benchmark objective, datasets, and solvers. The folder should have the
 # name `benchmark_utils`, and code defined inside will be importable using
-# the usual import syntax
+# the usual import syntax. To import external packages in this file, use a
+# `safe_import_context` named "import_ctx", as follows:
+
+from benchopt.utils import safe_import_context
+
+with safe_import_context() as import_ctx:
+    import numpy as np
+
 
 def gradient_ols(X, y, beta):
     return X.T @ (X @ beta - y)
+
+
+def value_ols(X, y, beta):
+    return 0.5 * np.mean((y - X @ beta) ** 2)
