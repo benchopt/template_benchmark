b01a9fead41dfc2dc996f6cfd8b19c6e100255fa
tommoral <thomas.moreau.2010@gmail.com>
2025-09-26 00:38:15 +0200

    FIX trailing context

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -1,13 +1,8 @@
 # `benchmark_utils` is a module in which you can define code to reuse in
 # the benchmark objective, datasets, and solvers. The folder should have the
 # name `benchmark_utils`, and code defined inside will be importable using
-# the usual import syntax. To import external packages in this file, use a
-# `safe_import_context` named "import_ctx", as follows:
-
-from benchopt.utils import safe_import_context
-
-with safe_import_context() as import_ctx:
-    import numpy as np
+# the usual import syntax.
+import numpy as np
 
 
 def gradient_ols(X, y, beta):
