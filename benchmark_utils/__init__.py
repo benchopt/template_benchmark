FIX Lipschitz normalization in GD + ordinary least-squares cost functions (#50)
Co-authored-by: Thomas Moreau <thomas.moreau.2010@gmail.com>

benchmark_utils/__init__.py
@@ -10,4 +10,4 @@ def gradient_ols(X, y, beta):
 
 
 def value_ols(X, y, beta):
-    return 0.5 * np.mean((y - X @ beta) ** 2)
+    return 0.5 * np.linalg.vector_norm(y - X @ beta) ** 2